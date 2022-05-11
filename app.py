6dc077ad50b271f1a8232e512f4c914a09779037
Kenny Nguyen <96954806+kenny-e-nguyen@users.noreply.github.com>
2022-05-11 17:07:53 -0400

    Add OAuth

--- app.py
+++ app.py
@@ -1,16 +1,77 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+from authlib.integrations.flask_client import OAuth
 
 
 app = Flask(__name__)
-
 app.secret_key = os.urandom(12).hex()
-# app.config['SERVER_NAME'] = 'localhost:5000'
+
+oauth = OAuth(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/twitter/')
+def twitter():
+    TWITTER_CLIENT_ID = os.environ.get('TWITTER_CLIENT_ID')
+    TWITTER_CLIENT_SECRET = os.environ.get('TWITTER_CLIENT_SECRET')
+    oauth.register(
+        name='twitter',
+        client_id=TWITTER_CLIENT_ID,
+        client_secret=TWITTER_CLIENT_SECRET,
+        request_token_url='https://api.twitter.com/oauth/request_token',
+        request_token_params=None,
+        access_token_url='https://api.twitter.com/oauth/access_token',
+        access_token_params=None,
+        authorize_url='https://api.twitter.com/oauth/authenticate',
+        authorize_params=None,
+        api_base_url='https://api.twitter.com/1.1/',
+        client_kwargs=None,
+    )
+    redirect_uri = url_for('twitter_auth', _external=True)
+    return oauth.twitter.authorize_redirect(redirect_uri)
+
+
+@app.route('/twitter/auth/')
+def twitter_auth():
+    token = oauth.twitter.authorize_access_token()
+    resp = oauth.twitter.get('account/verify_credentials.json')
+    profile = resp.json()
+    print(" Twitter User", profile)
+    return redirect('/')
+
+
+@app.route('/facebook/')
+def facebook():
+    FACEBOOK_CLIENT_ID = os.environ.get('FACEBOOK_CLIENT_ID')
+    FACEBOOK_CLIENT_SECRET = os.environ.get('FACEBOOK_CLIENT_SECRET')
+    oauth.register(
+        name='facebook',
+        client_id=FACEBOOK_CLIENT_ID,
+        client_secret=FACEBOOK_CLIENT_SECRET,
+        access_token_url='https://graph.facebook.com/oauth/access_token',
+        access_token_params=None,
+        authorize_url='https://www.facebook.com/dialog/oauth',
+        authorize_params=None,
+        api_base_url='https://graph.facebook.com/',
+        client_kwargs={'scope': 'email'},
+    )
+    redirect_uri = url_for('facebook_auth', _external=True)
+    return oauth.facebook.authorize_redirect(redirect_uri)
+
+
+@app.route('/facebook/auth/')
+def facebook_auth():
+    token = oauth.facebook.authorize_access_token()
+    resp = oauth.facebook.get(
+        'https://graph.facebook.com/me?fields=id,name,email,picture{url}'
+    )
+    profile = resp.json()
+    print("Facebook User ", profile)
+    return redirect('/')
 
 
 if __name__ == "__main__":
